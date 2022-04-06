54ebd145f73b598f2df55b61b107f59976902c52
Adm <admloja43@gmail.com>
2022-04-06 09:47:34 -0300

    v4

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Venda
 from .models import Produto
@@ -6,3 +7,7 @@ from .models import Produto
 def inicio(request):
     produtos= Produto.objects.order_by('idproduto')
     return render(request, 'shop/inicio.html', {'produtos':produtos})
+
+def pag2(request, pk):
+    post = get_object_or_404(Produto, pk=pk)
+    return render(request, 'shop/pag2.html', {'post': post})
